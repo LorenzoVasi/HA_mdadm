ae7a3c7e0c1de8bc077f969da83288a240639459
LorenzoVasi <48059088+LorenzoVasi@users.noreply.github.com>
2021-05-01 23:00:34 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,1 @@
+"""MDADM Raid Status"""
--- sensor.py
+++ sensor.py
@@ -4,13 +4,17 @@ def setup(hass, config):
     from homeassistant.helpers.entity import Entity # Importo libreria per creare entita
     import mdstat # Importo libreria MDSTAT installata da manifest.json
     import json # Importo libreria JSON per leggere dati JSON
-
+    from types import SimpleNamespace # Da vedere se disponibile su Home Assistant Core, altrimenti da importare 
 
     return True # Inizializzazione effettuata e completata
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
+
+    device = config[CONF_DEVICE]    
+    
     add_entities([mdadm_state()]) # Creo nuova entita chiamata sensor.mdadm_state
+
 
 class mdadm_state(Entity):
     def __init__(self):
@@ -28,8 +32,10 @@ class mdadm_state(Entity):
     def update(self):
         # Qui devo eseguire il codice da decifrare in JSON
 
-        jsonData = mdstat.parse()
-        jsonToPy = json.loads(jsonData)
+        data = json.dumps(mdstat.parse()) # Prendo il codice ricevuto da mdstat e lo trasformo in vero json
+        x = json.loads(data, object_hook=lambda d: SimpleNamespace(**d)) # Trasformo il risultato JSON in una classe
         
+        
+        self._state = x.devices.md0.active
 
     
