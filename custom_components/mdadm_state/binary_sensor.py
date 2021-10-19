ab4c687b535a84cf665b5a15fc40cd4d106e794d
LorenzoVasi <48059088+LorenzoVasi@users.noreply.github.com>
2021-10-19 19:26:37 +0200

    "ReSync Status" Update
    Added ReSync Status of RAID Devices [Percentual Progress, Time Remaining, Transfering Speed, Type of Operation]

--- custom_components/mdadm_state/binary_sensor.py
+++ custom_components/mdadm_state/binary_sensor.py
@@ -23,6 +23,10 @@ class mdadm(Entity):
         self._status_disks_not_working = None
         self._status_synced = None
         self._disks_number = None
+        self._resync_operation = None
+        self._resync_progress = None
+        self._resync_finish = None
+        self._resync_speed = None
 
     @property
     def name(self): # entity name
@@ -39,7 +43,11 @@ class mdadm(Entity):
             "raid-type": self._personality,
             "disks_number": self._disks_number,
             "disks_not_working": self._status_disks_not_working,
-            "sync": self._status_synced
+            "sync": self._status_synced,
+    	    "resync_operation": self._resync_operation,
+            "resync_percentual_progress": self._resync_progress,
+            "resync_remaining_time": self._resync_finish,
+            "resync_speed": self._resync_speed
         }
         return attributes
         
@@ -67,3 +75,7 @@ class mdadm(Entity):
         self._personality = device_config.personality
         self._status_disks_not_working = device_config.status.raid_disks - device_config.status.non_degraded_disks
         self._disks_number = device_config.status.raid_disks
+        self._resync_operation = device_config.resync.operation
+        self._resync_progress = device_config.resync.progress
+        self._resync_finish = device_config.resync.finish
+        self._resync_speed = device_config.resync.speed
